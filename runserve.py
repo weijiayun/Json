2961c08df8f77f8449f52d3406c47c5c907d82f0
jiayun.wei <jiayun.wei@dfc.sh>
2016-08-15 19:53:34 +0800

    new stage in handsontable

--- runserve.py
+++ runserve.py
@@ -1,7 +1,7 @@
 #!/opt/Apps/local/Python/anaconda/bin/python2.7
 __author__ = 'jiayun.wei'
 
-from typeapp import app
+from TemplateApp import app
 def run():
     app.debug = True
     app.run()
--- web/runserve.py
+++
@@ -1,8 +0,0 @@
-#!/opt/Apps/local/Python/anaconda/bin/python2.7
-__author__ = 'jiayun.wei'
-
-from typeapp import app
-def run():
-    app.debug = True
-    app.run()
-run()
